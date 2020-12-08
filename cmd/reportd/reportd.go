b7d3037062b78e2fba89e44c27a41e1cf4f857a2
John Sommerville <jsommerville@untangle.com>
2020-12-07 17:31:48 -0700

    basic zmq subscriber implementation to try and listen on static socket

--- cmd/reportd/reportd.go
+++ cmd/reportd/reportd.go
@@ -4,6 +4,7 @@ import (
 	"os"
 	"time"
 
+	zmq "github.com/pebbe/zmq4"
 	"github.com/untangle/packetd/services/logger"
 )
 
@@ -13,7 +14,13 @@ var routineWatcher = make(chan int)
 func main() {
 	logger.Info("Starting up reportd...\n")
 
-	go eventListener()
+	socket, err := setupZmqSocket()
+
+	if err != nil {
+		logger.Warn("Unable to setup ZMQ sockets.")
+	}
+
+	go eventListener(socket)
 
 	go eventLogger()
 
@@ -31,23 +38,55 @@ func main() {
 }
 
 // eventListener is used to listen for ZMQ events being published
-func eventListener() {
+func eventListener(soc *zmq.Socket) {
 	routineWatcher <- +1
 	logger.Info("Starting up a theoretical goroutine for event listening...\n")
-	time.Sleep(30 * time.Second)
-	go finishRoutine()
+	defer startFinishRoutineThread()
+
+	for {
+		msg, err := soc.RecvMessage(0)
+		if err != nil {
+			logger.Warn("Unable to receive messages: %s...\n", err)
+			break
+		}
+
+		logger.Info("Got some data here, topic: %s, message: %s\n", msg[0], msg[1])
+	}
 }
 
 // eventLogger is used to log events to the database
 func eventLogger() {
 	routineWatcher <- +1
+	defer startFinishRoutineThread()
 
 	logger.Info("Starting up a theoretical goroutine for event logging...\n")
 	time.Sleep(30 * time.Second)
+}
+
+// startFinishRoutineThread is a function to simplify how we can defer calling finishRoutine() at the top of a function,
+// instead of having to always call it at the end of a routine
+func startFinishRoutineThread() {
 	go finishRoutine()
 }
 
 // finishRoutine is called at the end of a running go routine to empty the channel watcher
 func finishRoutine() {
 	routineWatcher <- -1
+}
+
+// setupZmqSocket prepares a zmq socket for listening to events
+func setupZmqSocket() (soc *zmq.Socket, err error) {
+	subscriber, err := zmq.NewSocket(zmq.SUB)
+
+	if err != nil {
+		logger.Err("Unable to open ZMQ socket... %s\n", err)
+		return nil, err
+	}
+
+	defer subscriber.Close()
+
+	subscriber.Connect("tcp://localhost:5561")
+	subscriber.SetSubscribe("packetd-events")
+
+	return subscriber, nil
 }
