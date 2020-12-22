d759913002c3efcdeb9adea3d10ee293d02170b2
John Sommerville <jsommerville@untangle.com>
2020-12-22 11:29:02 -0700

    introduce context done channels and RoutineContextGroups to the monitor

--- services/monitor/monitor.go
+++ services/monitor/monitor.go
@@ -1,6 +1,7 @@
 package monitor
 
 import (
+	"context"
 	"sync"
 	"time"
 
@@ -8,9 +9,9 @@ import (
 )
 
 var routineInfoWatcher = make(chan *routineInfo)
-var serviceShutdown = make(chan bool, 1)
 var activeRoutines = make(map[string]bool)
 var activeRoutinesMutex = &sync.RWMutex{}
+var monitorRelation = RoutineContextGroup{}
 
 // routineInfo is a struct used to signal routine events
 type routineInfo struct {
@@ -27,13 +28,24 @@ const (
 	end
 )
 
+// RoutineContextGroup is a collection of contexts and cancels that are associated
+// with one specific routine group (ie: all local reporting contexts)
+// This can be used with CancelContexts to send a ctx.Done() to all routines
+// within a specific group
+type RoutineContextGroup struct {
+	Name     string
+	Contexts map[string]context.Context
+	Cancels  map[string]context.CancelFunc
+}
+
 // Startup is called to startup the monitor service
 func Startup() {
 	logger.Info("Starting routine monitor service...\n")
 	routineInfoWatcher = make(chan *routineInfo)
 	activeRoutines = make(map[string]bool)
+	monitorRelation = CreateRoutineContextRelation(context.Background(), "monitor", []string{"routineInfoWatcher"})
 
-	go monitorRoutineEvents()
+	go monitorRoutineEvents(monitorRelation.Contexts["routineInfoWatcher"])
 
 }
 
@@ -72,32 +84,65 @@ func RoutineError(routineName string) {
 	}
 }
 
+// CreateRoutineContextRelation will create a collection of routine contexts and cancels that are related to routineNames passed as input
+// This object is used to store context relations among many contexts, allowing for closing all contexts at once if needed
+func CreateRoutineContextRelation(ctx context.Context, name string, routineNames []string) RoutineContextGroup {
+	returnCtxs := make(map[string]context.Context)
+	returnCancels := make(map[string]context.CancelFunc)
+
+	for _, rtName := range routineNames {
+		var thisCtx, thisCleaner = context.WithCancel(ctx)
+
+		returnCtxs[rtName] = thisCtx
+		returnCancels[rtName] = thisCleaner
+
+	}
+
+	return RoutineContextGroup{Name: name, Contexts: returnCtxs, Cancels: returnCancels}
+}
+
+// CancelContexts is a routine that will wait for a cancel call at the top level context
+// and then deliver a cancel call to all downstream contexts passed in the cancels param
+func CancelContexts(relation RoutineContextGroup) {
+	var wg sync.WaitGroup
+
+	for _, f := range relation.Cancels {
+		wg.Add(1)
+		go func(f func(), wgs *sync.WaitGroup) {
+			defer wgs.Done()
+			f()
+		}(f, &wg)
+	}
+
+	wg.Wait()
+	logger.Info("All routines associated with %s should be cancelled...\n", relation.Name)
+
+}
+
 // Shutdown is called to close the routine monitor
 func Shutdown() {
 	// Set shutdown channel for this service
-	logger.Info("Sending service shutdown channel to monitor routines...")
-
-	serviceShutdown <- true
-
+	logger.Info("Shutting down monitor service...\n")
+	CancelContexts(monitorRelation)
 }
 
 // montitorRoutineEvents is a routine that monitors the routineInfoWatcher queue for any routine events to act on
 // THIS IS A ROUTINE FUNCTION
-func monitorRoutineEvents() {
+func monitorRoutineEvents(ctx context.Context) {
 
 	// Read the routineInfoWatcher channel for any Error types
 	for {
 		select {
-		case <-serviceShutdown:
-			logger.Info("Stopping routine monitor\n")
-			return
 		case rtEvt := <-routineInfoWatcher:
 			if rtEvt.Action == err {
 				logger.Info("Acting on this event: %v\n", rtEvt)
 				handleRoutineWatcherEvents()
 			}
-		case <-time.Tick(60 * time.Hour):
+		case <-time.Tick(60 * time.Second):
 			logger.Info("There are %v monitored routines.\n", len(activeRoutines))
+		case <-ctx.Done():
+			logger.Info("Stopping routine monitor\n")
+			return
 		}
 	}
 }
